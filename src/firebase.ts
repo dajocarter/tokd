c9e1221883ffd81e019679e6ecbb66ffe5905d2f
David Carter <dajocarter@gmail.com>
2026-02-15 15:49:44 -0800

    add db for firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -21,6 +21,7 @@
 
 import { initializeApp, getApps } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -30,14 +31,16 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 }
 
 let app
 if (!getApps().length) {
-  app = initializeApp(firebaseConfig as any)
+  app = initializeApp(firebaseConfig)
 } else {
   app = getApps()[0]
 }
 
 export const auth = getAuth(app)
+export const database = getDatabase(app)
 export default app
