0655affd120dc9be781ac73f7d6dc0a65930b5a9
David Carter <dajocarter@gmail.com>
2026-02-15 16:30:07 -0800

    create a logout button on the account page

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -2,13 +2,25 @@ import {
   IonContent,
   IonHeader,
   IonPage,
+  IonFooter,
   IonTitle,
+  IonButton,
   IonToolbar,
 } from '@ionic/react'
 import ExploreContainer from '../../components/ExploreContainer'
+import { useAuth } from '../../context/AuthContext'
 import './index.css'
 
 export default function Account() {
+  const { signOut } = useAuth()
+
+  const handleLogout = async () => {
+    try {
+      await signOut()
+    } catch (err) {
+      console.error('Logout failed', err)
+    }
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -24,6 +36,13 @@ export default function Account() {
         </IonHeader>
         <ExploreContainer name='Account page' />
       </IonContent>
+      <IonFooter>
+        <IonToolbar>
+          <IonButton color='danger' expand='block' onClick={handleLogout}>
+            Logout
+          </IonButton>
+        </IonToolbar>
+      </IonFooter>
     </IonPage>
   )
 }
